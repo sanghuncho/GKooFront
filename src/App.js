43d5805a26703d2341469127b857afc0bce4d20e
sanghuncho <sanghun.cho@isys.de>
2018-11-19 14:47:40 +0100

    Eine Navigation ist hinzugefuegt.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import  logo  from './assets/hausbank_logo.jpg';
+import { AppContainer, Navigation, Body, Title } from "./container";
+import { AppNavigation } from "./AppNavigation";
+import { MemoryRouter as Router, Route, Switch } from "react-router-dom";
+
+var green = '#61a556';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav class="navbar" style={{background:green, marginLeft:-16, height:50}}>
+         <a class="navbar-brand" href="#">
+             <img src={ logo } width="180" height="50" alt=""/>
+         </a>
+        </nav>
+
+      <AppContainer>
+        <Navigation>
+          {/* <Title> React SideNav </Title> */}
+          <AppNavigation />
+        </Navigation>
+      {/* <Body>
+        <Switch> */}
+          {/* <Route path="//basic" component={Basic} />
+          <Route path="//renderitems" component={RenderItems} />
+          <Route path="//renderitems2" component={RenderItems2} />
+          <Route path="/" exact component={Home} /> */}
+      {/*  </Switch>
+       </Body>*/}
+      </AppContainer> 
+
       </div>
+      
     );
   }
 }
 
 export default App;
+
+// export default App = () => {
+//   return class SideNavApp extends React.Component {
+//     render() {
+//       return (
+//         <Router>
+//           <App />
+//         </Router>
+//       );
+//     }
+//   };
+// };
