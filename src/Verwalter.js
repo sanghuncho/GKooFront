df24112e6b576ddeeb5d60e55c964ec13a65b7af
sanghuncho <sanghun.cho@isys.de>
2018-11-21 17:00:39 +0100

    Eine Tabelle ist hinzugefuegt und intern funktioniert.

--- src/Verwalter.js
+++ src/Verwalter.js
@@ -1,10 +1,18 @@
 import * as React from "react";
 import { FormGroup, Form, ControlLabel, FormControl, Button, InputGroup} from 'react-bootstrap';
 import styled from "styled-components";
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 const VewalterFrom = styled.div`
-  margin-top: 10px;
-  margin-left: 10px;
+  margin-top: 50px;
+  margin-left: 50px;
+`;
+
+const VerwalterTableWidth = styled.div`
+  margin-top: 50px;
+  margin-left: 50px;
+  width: 1000px;
 `;
 
 var green = '#61a556';
@@ -49,7 +57,8 @@ export class Verwalter extends React.Component {
         <Button style={{ background: '#61a556',color: 'white', marginLeft:'2px' }} onClick={this.handleClick}>Suchen</Button>      
      </Form>
      </VewalterFrom>
-     <VerwalterBody/>
+     {/* <VerwalterBody/> */}
+     <VerwalterTable/>
      </div>
     );}
 }
@@ -57,9 +66,34 @@ export class Verwalter extends React.Component {
 export default Verwalter;
 
 const VerwalterBody = () => (
-  <div className='verwalter'>
+  
+  <div>
     <h1>Verwalter</h1>
     <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
     <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
   </div>
+  
+);
+
+var products = [{
+  id: 1,
+  name: "Product1",
+  price: 120
+}, {
+  id: 2,
+  name: "Product2",
+  price: 80
+}];
+
+const VerwalterTable = () => (
+  <div className='verwalterTable'>
+  <VerwalterTableWidth>
+   <BootstrapTable data={ products }  hover condensed>
+      <TableHeaderColumn width='50' isKey dataField='id'>Objekt ID</TableHeaderColumn>
+      <TableHeaderColumn width='50' dataField='name'>Verwalter Nummer</TableHeaderColumn>
+      <TableHeaderColumn width='50' dataField='price'>Bezeichnung</TableHeaderColumn>
+  </BootstrapTable>
+  </VerwalterTableWidth>
+  </div>
+
 );
