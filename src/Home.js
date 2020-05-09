e2b01babcf1ed53bfe64ce21136bc4458fe3391b
sanghuncho <moondrive@hotmail.co.kr>
2020-05-09 11:07:25 +0200

    home component shows the info

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,10 @@ import { AppNavbar } from './AppNavbar'
 import { Navbar, Nav, Form, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { signOut, exchange } from 'react-icons-kit/fa/'
 import { Icon as BaseIcon } from "react-icons-kit";
+import { Image } from 'react-bootstrap';
+import GKoo_Service_Info  from './assets/GKoo_Service_Info.jpg';
 
 const Icon = props => <BaseIcon size={18} icon={props.icon} />;
-//import styled from "styled-components";
 
 
 export class Home extends React.Component {
@@ -13,26 +14,9 @@ export class Home extends React.Component {
     return(
     <div className='home'>
         <AppNavbar>
-            {/* <Form inline>
-                    <OverlayTrigger
-                        key='bottom'
-                        placement='bottom'
-                        overlay={
-                            <Tooltip id='logout'>
-                            로그아웃
-                            </Tooltip>
-                        }
-                        >
-                        <Button variant="secondary"
-                        onClick={(e) => this.handleLogin(e)}
-                        >
-                        <Icon icon={ signOut } />
-                        </Button>
-                        </OverlayTrigger>
-            </Form> */}
+           
         </AppNavbar>
-        <h1>Home Site</h1>
-        <p> Feel free to browse around and learn more about me.</p>
+        <Image src={GKoo_Service_Info} style={{ width: '600px', marginLeft:'20%', marginTop:'20px'}}/>
     </div>
     );}
 }
